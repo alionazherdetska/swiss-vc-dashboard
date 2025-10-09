60d55cd746e43a8f275afcc1339a08009eaa7f0f
alionazherdetska <alionazhv@gmail.com>
2025-10-09 17:33:40 +0200

    fix imports

--- src/components/charts/shared/index.js
+++ src/components/charts/shared/index.js
@@ -3,7 +3,6 @@ export { default as BaseExpandableChart } from "./BaseExpandableChart";
 export { default as ChartControls } from "./ChartControls";
 export { default as ChartHeader } from "./ChartHeader";
 export { default as ExportButton } from "./ExportButton";
-export { DualChartLayout, SingleChartLayout } from "./ChartLayouts";
 export { default as D3MultiSeriesChart } from "./D3MultiSeriesChart";
 export { default as useD3Chart } from "./useD3Chart";
 
