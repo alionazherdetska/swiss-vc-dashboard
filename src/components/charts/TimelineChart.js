5c632e90e89777d59a41584d815daea448fb20a1
alionazherdetska <alionazhv@gmail.com>
2025-11-09 12:21:50 +0100

    fixed linting

--- src/components/charts/TimelineChart.js
+++ src/components/charts/TimelineChart.js
@@ -1,5 +1,4 @@
 import D3AreaChart from "./shared/D3AreaChart";
-import ResponsiveD3Container from "./shared/ResponsiveD3Container";
 
 export const TimelineChart = ({ data, showVolume = false, title, yLabel }) => {
   const chartKey = showVolume ? "volume" : "count";
@@ -7,18 +6,18 @@ export const TimelineChart = ({ data, showVolume = false, title, yLabel }) => {
   const chartLabel = yLabel || defaultY;
 
   return (
-        <D3AreaChart
-          data={data}
-          dataKey={chartKey}
-          margin={{ top: 50, right: 50, left: 60, bottom: 60 }}
-          strokeColor="#E84A5F"
-          strokeWidth={2}
-          fillColor="#E84A5F"
-          fillOpacity={0.8}
-          gridColor="#E2E8F0"
-          axisColor="#4A5568"
-          yAxisLabel={chartLabel}
-          showVolume={showVolume}
-        />
+    <D3AreaChart
+      data={data}
+      dataKey={chartKey}
+      margin={{ top: 50, right: 50, left: 60, bottom: 60 }}
+      strokeColor="#E84A5F"
+      strokeWidth={2}
+      fillColor="#E84A5F"
+      fillOpacity={0.8}
+      gridColor="#E2E8F0"
+      axisColor="#4A5568"
+      yAxisLabel={chartLabel}
+      showVolume={showVolume}
+    />
   );
 };
