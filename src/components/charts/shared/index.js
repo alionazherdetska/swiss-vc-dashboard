7874819405d38eb05e330a3f94e2dd2208d2333f
alionazherdetska <alionazhv@gmail.com>
2025-11-21 20:15:48 +0100

    removed redundant code

--- src/components/charts/shared/index.js
+++
@@ -1,9 +0,0 @@
-// Chart components barrel export
-export { default as TimelineChart, TimelineChart } from "./TimelineChart";
-export { default as CantonAnalysisChart } from "./CantonAnalysisChart";
-export { default as GenderAnalysisChart } from "./GenderAnalysisChart";
-export { default as QuarterlyAnalysisChart } from "./QuarterlyAnalysisChart";
-export { default as PhaseAnalysisChart } from "./PhaseAnalysisChart";
-
-// Re-export shared components for convenience
-export * from "./shared";
